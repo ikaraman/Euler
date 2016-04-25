ce174f97947ad80d7f15fa7d4fffb7075ecfd3f9
ikaraman <ikaraman@ya.ru>
2016-04-25 13:16:17 +1200

    Problem13 solved.

--- src/com/problems/Problem13/Problem13.java
+++ src/com/problems/Problem13/Problem13.java
@@ -2,22 +2,50 @@ package com.problems.Problem13;
 
 import com.problems.Problem11.FileReader;
 
+import java.math.BigInteger;
+
 public class Problem13 {
     public static void main(String[] args) {
-        String pathToFile = "c:/javafile3.txt";
+
+        //read the file
+        String pathToFile = "C:/Users/ikaraman/IdeaProjects/Euler/src/com/problems/Problem13/fileToRead.txt";
         String file;
         file = FileReader.readAndReturn(pathToFile);
-        System.out.println(file);
-    }
+        System.out.println("File is: " + file);
 
-    public static Long sum(Long num1, Long num2){
-        Long result = 0L;
-        result = num1 + num2;
-        return result;
-    }
+        //create placeholder array for 50-digits numbers
+        BigInteger [] sums = new BigInteger[100];
+        String number = "";
+        int n = 0;
 
-    public static void fileIntoArray (String path){
+        //loop to save all numbers to array
+        for ( int i = 0; i < file.length(); i++){
+            number = number + String.valueOf(file.charAt(i));
 
+            //if space found, record number & proceed further
+            if ( i+1 < file.length() && Character.getNumericValue(file.charAt(i+1)) == -1 && n < 100 ) {
+                    sums[n] = new BigInteger(number);
+                    number = "";
+                    n++;
+                    i++;
+                }
+            }
+
+        //add all numbers from the array to each other
+        BigInteger total = BigInteger.valueOf(0);
+        for (int i = 0; i < sums.length; i++) {
+            total = total.add(sums[i]);
+        }
+        System.out.println("Sum of all 50-digits numbers is: " + total);
+        String temp = total.toString();
+
+        //read first 10 digits from the sum
+        String temp2 = "";
+        for (int i = 0; i < 10; i++) {
+            temp2 = temp2 + String.valueOf(temp.charAt(i));
+        }
+        System.out.println("First 10 digits of the sum is: " + temp2);
     }
 }
+
 
